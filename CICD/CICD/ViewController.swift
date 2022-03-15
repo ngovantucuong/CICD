ec3e8e83b19ea960d02d0661fcf237f0d275df32
cuongnvt <cuongnvt@tech.est-rouge.com>
2022-03-15 21:55:52 +0700

    Init project

---
+++ CICD/CICD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICD
+//
+//  Created by ER_Macbook_225 on 15/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
